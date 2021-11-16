086c2ea41aaec459135c353ad0f9f18c2ad2c904
Qrzysio <krzysiek@chojnowski.us>
2021-11-16 22:27:15 +0100

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# safeMailto" 
+# safeMailto
+
+`safe_mailto()` taken from [CodeIgniter 4](https://github.com/codeigniter4/CodeIgniter4).
---
+++ src/safeMailto.function.php
@@ -0,0 +1,91 @@
+<?php
+
+if (! function_exists('safe_mailto')) {
+    /**
+     * Encoded Mailto Link
+     *
+     * Create a spam-protected mailto link written in Javascript
+     *
+     * @param string $email      the email address
+     * @param string $title      the link title
+     * @param mixed  $attributes any attributes
+     */
+    function safe_mailto(string $email, string $title = '', $attributes = ''): string
+    {
+        if (trim($title) === '') {
+            $title = $email;
+        }
+
+        $x = str_split('<a href="mailto:', 1);
+
+        for ($i = 0, $l = strlen($email); $i < $l; $i++) {
+            $x[] = '|' . ord($email[$i]);
+        }
+
+        $x[] = '"';
+
+        if ($attributes !== '') {
+            if (is_array($attributes)) {
+                foreach ($attributes as $key => $val) {
+                    $x[] = ' ' . $key . '="';
+
+                    for ($i = 0, $l = strlen($val); $i < $l; $i++) {
+                        $x[] = '|' . ord($val[$i]);
+                    }
+
+                    $x[] = '"';
+                }
+            } else {
+                for ($i = 0, $l = mb_strlen($attributes); $i < $l; $i++) {
+                    $x[] = mb_substr($attributes, $i, 1);
+                }
+            }
+        }
+
+        $x[] = '>';
+
+        $temp = [];
+
+        for ($i = 0, $l = strlen($title); $i < $l; $i++) {
+            $ordinal = ord($title[$i]);
+
+            if ($ordinal < 128) {
+                $x[] = '|' . $ordinal;
+            } else {
+                if (empty($temp)) {
+                    $count = ($ordinal < 224) ? 2 : 3;
+                }
+
+                $temp[] = $ordinal;
+
+                if (count($temp) === $count) { // @phpstan-ignore-line
+                    $number = ($count === 3) ? (($temp[0] % 16) * 4096) + (($temp[1] % 64) * 64) + ($temp[2] % 64) : (($temp[0] % 32) * 64) + ($temp[1] % 64);
+                    $x[]    = '|' . $number;
+                    $count  = 1;
+                    $temp   = [];
+                }
+            }
+        }
+
+        $x[] = '<';
+        $x[] = '/';
+        $x[] = 'a';
+        $x[] = '>';
+
+        $x = array_reverse($x);
+
+        // improve obfuscation by eliminating newlines & whitespace
+        $output = '<script type="text/javascript">'
+                . 'var l=new Array();';
+
+        foreach ($x as $i => $value) {
+            $output .= 'l[' . $i . "] = '" . $value . "';";
+        }
+
+        return $output . ('for (var i = l.length-1; i >= 0; i=i-1) {'
+                . "if (l[i].substring(0, 1) === '|') document.write(\"&#\"+unescape(l[i].substring(1))+\";\");"
+                . 'else document.write(unescape(l[i]));'
+                . '}'
+                . '</script>');
+    }
+}
